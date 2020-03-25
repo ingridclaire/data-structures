c21c96a222f1f4928cbe0787bb6c94c224eb4b52
Ingrid Carlson <ingridclaire@gmail.com>
2020-03-25 16:41:49 -0700

    implemented queue and stack for functional types! Woohoo!

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,7 @@
 var Queue = function() {
   var someInstance = {};
+  var start = 0;
+  var end = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -7,12 +9,25 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[end] = value;
+    end++;
+    console.log(start);
+    return end - start;
   };
 
   someInstance.dequeue = function() {
+    var currentVal = storage[start];
+    delete storage[start];
+    start++;
+    return currentVal;
   };
 
   someInstance.size = function() {
+    if ((end - start) <= 0) {
+      return 0;
+    } else {
+      return end - start;
+    }
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,28 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[size] = value;
+    size++;
+    return size;
   };
 
   someInstance.pop = function() {
+    var currentVal = storage[size - 1];
+    delete storage[size - 1];
+    size--;
+    return currentVal;
   };
 
   someInstance.size = function() {
+    if (size <= 0) {
+      return 0;
+    } else {
+    return size;
+    }
   };
 
   return someInstance;
