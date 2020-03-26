90399ca9a2ee1af539078645a66e4bd46d2d7990
Eric Song <eric.song@columbia.edu>
2020-03-26 16:15:55 -0700

    Completed sprint one

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,29 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.start = 0;
+    this.end = 0;
   }
 
+  enqueue(value) {
+    this.storage[this.end] = value;
+    this.end++;
+    return this.end - this.start;
+  }
+
+  dequeue() {
+    var currentVal = this.storage[this.start];
+    delete this.storage[this.start];
+    this.start++;
+    return currentVal;
+  }
+
+  size() {
+    if ((this.end - this.start) <= 0) {
+      return 0;
+    } else {
+      return this.end - this.start;
+    }
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,30 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.length = 0;
   }
+
+  push(value) {
+    this.storage[this.length] = value;
+    this.length++;
+    return this.length;
+  }
+
+  pop() {
+    var currentVal = this.storage[this.length - 1];
+    delete this.storage[this.length - 1];
+    this.length--;
+    return currentVal;
+  }
+
+  size() {
+    if (this.length <= 0) {
+      return 0;
+    } else {
+      return this.length;
+    }
+  }
+
 
 }
